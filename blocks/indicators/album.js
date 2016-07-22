7e638d762d6d931b49c076991f41e85e83a66296
stsdc <stanislaw.dac@gmail.com>
2016-07-23 00:36:29 +0200

    remove prev & next. seekbar 3px up

--- blocks/indicators/album.js
+++ blocks/indicators/album.js
@@ -2,14 +2,15 @@
   'use strict';
 
   let qsa = document.querySelectorAll.bind(document);
+  let log = window.vkScrobbler.log;
   const DEFAULT_COVER =  chrome.extension.getURL("blocks/indicators/__icon/indicators__defaultCover.png");
 
   let album = {
     // returning all selectors with particular classname
-    getPlayButtonArray: (className) => qsa(className),
+    getButtonsArray: (className) => qsa(className),
 
     setAlbumCover: function(imgUrl) {
-      let playButtonArray = this.getPlayButtonArray(".audio_page_player_play");
+      let playButtonArray = this.getButtonsArray(".audio_page_player_play");
       let bgImg = "url('" + DEFAULT_COVER + "')";
       if (imgUrl) {
         console.log(bgImg);
@@ -20,9 +21,17 @@
 
     modifyPlayer: function() {
       // modifying VK player in dropdown and audio page
-      let playButtonArray = this.getPlayButtonArray(".audio_page_player_play");
+      let playButtonArray = this.getButtonsArray(".audio_page_player_play");
 
       [].slice.call(playButtonArray).map((div) => div.firstElementChild.classList.add("playIcon"));
+      
+      // remove prev and next
+      this.removeButtons(".audio_page_player_prev, .audio_page_player_next");
+    },
+
+    removeButtons: function (classNames) {
+      let buttons = [].slice.call(this.getButtonsArray(classNames));
+      buttons.map((div) => div.remove());
     },
   };
   window.vkScrobbler.album = album;
